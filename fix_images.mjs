16889bc6e75706c22d12da382c4e03d155ed6d16
vysh-x <140082966+vysh-x@users.noreply.github.com>
2026-09-04 20:09:00 +0530

    Use local images for all products

--- fix_images.mjs
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let data = fs.readFileSync('js/mockData.js', 'utf8');
-data = data.replace(/"(MAT-\d+)":\s*"https:\/\/picsum\.photos\/seed\/picsum\/300\/400"/g, '"$1": "https://picsum.photos/seed/$1/300/400"');
-fs.writeFileSync('js/mockData.js', data);
--- fix_referrer.mjs
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let render = fs.readFileSync('js/render.js', 'utf8');
-render = render.replace(/<img class="product-image" src="\$\{product\.image\}" alt="\$\{product\.title\}" \/>/g, '<img class="product-image" src="${product.image}" alt="${product.title}" referrerpolicy="no-referrer" />');
-fs.writeFileSync('js/render.js', render);
